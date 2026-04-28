261734f944f835a12930e24e985bd5d262a236e6
cookcaptain57-cpu <cookcaptain57@gmail.com>
2026-04-28 00:10:34 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA69m0Jq4GzSxOeUr6ZzumlrSH9wcGy0p0",
+  authDomain: "ecdis-route-finder.firebaseapp.com",
+  projectId: "ecdis-route-finder",
+  storageBucket: "ecdis-route-finder.firebasestorage.app",
+  messagingSenderId: "636056685819",
+  appId: "1:636056685819:web:20d9dadb661135123eb45e"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
