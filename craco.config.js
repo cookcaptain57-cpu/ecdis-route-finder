f9a5a539b359bd894c9e7e94d0ac8cad7149d7f1
cookcaptain57-cpu <cookcaptain57@gmail.com>
2026-06-10 11:07:43 +0800

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,33 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      webpackConfig.module.rules.push({
-        test: /\.m?js/,
+      // Fix: allow dynamic import() in .mjs files from node_modules
+      webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
+        if (rule.oneOf) {
+          rule.oneOf = rule.oneOf.map(oneOfRule => {
+            if (oneOfRule.type === 'asset/resource') return oneOfRule;
+            return {
+              ...oneOfRule,
+              resolve: {
+                ...oneOfRule.resolve,
+                fullySpecified: false,
+              },
+            };
+          });
+        }
+        return rule;
+      });
+
+      // Add explicit rule for .mjs files
+      webpackConfig.module.rules.unshift({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
         resolve: {
           fullySpecified: false,
         },
       });
+
       return webpackConfig;
     },
   },
