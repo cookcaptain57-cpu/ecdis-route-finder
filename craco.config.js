d02910ce01f814500332ca42fcf466bbaf7b8632
cookcaptain57-cpu <cookcaptain57@gmail.com>
2026-06-10 11:39:14 +0800

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Fix: allow dynamic import() in .mjs files from node_modules
+
+      // Fix 1: Set target to support dynamic import()
+      webpackConfig.target = ['web', 'es5'];
+
+      // Fix 2: Allow .mjs files from node_modules
+      webpackConfig.module.rules.unshift({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        },
+      });
+
+      // Fix 3: Disable fullySpecified on all rules
       webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
         if (rule.oneOf) {
           rule.oneOf = rule.oneOf.map(oneOfRule => {
@@ -18,15 +32,23 @@ module.exports = {
         return rule;
       });
 
-      // Add explicit rule for .mjs files
-      webpackConfig.module.rules.unshift({
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
-        resolve: {
-          fullySpecified: false,
+      // Fix 4: Tell webpack to handle ESM modules from node_modules
+      webpackConfig.resolve = {
+        ...webpackConfig.resolve,
+        extensionAlias: {
+          '.js': ['.js', '.ts', '.tsx'],
         },
-      });
+        fullySpecified: false,
+      };
+
+      // Fix 5: Exclude problematic node_modules from being transpiled as scripts
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        environment: {
+          ...webpackConfig.output?.environment,
+          dynamicImport: true,
+        },
+      };
 
       return webpackConfig;
     },
